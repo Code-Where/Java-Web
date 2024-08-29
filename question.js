c8b8f8f66307176418cc06d04e285f502b499e5c
Code-Where <dhawana388@gmail.com>
2024-08-29 17:42:12 +0530

    new

---
+++ question.js
@@ -0,0 +1,30 @@
+const quesData = {
+    "questions": [
+        {
+            "id": 1,
+            "description": "Enter the Matrix of rows and columns entered by the user and print in matrix format",
+            "difficulty": "Easy",
+            "topic": 0,
+            "sample_input": null,
+            "sample_output": "hello world!",
+            "solution": {
+                "language": "Java",
+                "code": "public static void main(String[] args){}",
+                "explanation": "Explaining the above code"
+            }
+        },
+        {
+            "id": 2,
+            "description": "Program to Add Two Matrices",
+            "difficulty": "Easy",
+            "topic": 0,
+            "sample_input": null,
+            "sample_output": "hello world!",
+            "solution": {
+                "language": "Java",
+                "code": "public static void main(String[] args){}",
+                "explanation": "Explaining the above code"
+            }
+        }
+    ]
+}
