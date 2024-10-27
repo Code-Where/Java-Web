2ebd57b04bb95f3032a30e571d851fedf1b61050
Code-Where <dhawana388@gmail.com>
2024-10-27 15:04:29 +0530

    cc

--- learning.js
+++ learning.js
@@ -16,5 +16,43 @@ const phpTopics = [
             "Local Servers" : 4,
             "Dynamic Website" : 5
         }
-    }
+    },
+    {
+        "id" : 2,
+        "heading" : "Installation",
+        "subTopics" : {
+            "Installing WAMP" : 6,
+            "Configuring PHP Environment" : 7,
+            "Embedding PHP code in Web Pages" : 8
+        }
+    },
+    {
+        "id" : 3,
+        "heading" : "Php Basics",
+        "subTopics" : {
+            "Tokens" : 9,
+            "Variables" : 10,
+            "Variable Scope" : 11,
+            "Constants" : 12,
+            "Data Types" : 13,
+            "Operators" : 14,
+            "Comments" : 15,
+        }
+    },
+    {
+        "id" : 4,
+        "heading" : "Printing Statements",
+        "subTopics" : {
+            "echo" : 6,
+            "print" : 7,
+        }
+    },
+    {
+        "id" : 5,
+        "heading" : "Control Structures",
+        "subTopics" : {
+            "Branching Statements" : 6,
+            "print" : 7,
+        }
+    },
 ]
