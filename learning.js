7c0e7ace823ce5096a3c49809719afdb7245a7b0
Code-Where <dhawana388@gmail.com>
2024-10-27 14:42:56 +0530

    php  addition

---
+++ learning.js
@@ -0,0 +1,20 @@
+const phpTopics = [
+    {
+        "id" : 0,
+        "heading" : "Introduction to PHP",
+        "subTopics" : {
+            "PHP" : 0,
+            "Static Website" : 1,
+            "Dynamic Website" : 2
+        }
+    },
+    {
+        "id" : 1,
+        "heading" : "Understanding Web Servers",
+        "subTopics" : {
+            "Introduction" : 3,
+            "Local Servers" : 4,
+            "Dynamic Website" : 5
+        }
+    }
+]
